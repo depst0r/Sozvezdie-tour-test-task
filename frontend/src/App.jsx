4bc4fb3b249a97ebf5f168dbd35944a330f877ad
depst0r <depstor90@gmail.com>
2023-05-12 16:32:26 +0300

    card-style-bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Cards } from "./Components/Cards/Cards";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="container">
-      <Cards />
-    </div>
+    <Container>
+      <Row>
+        <Cards />
+      </Row>
+    </Container>
   );
 }
 
--- frontend/src/Components/Cards/Cards.jsx
+++ frontend/src/Components/Cards/Cards.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Spinner } from "../Spinner/Spinner";
+import Card from "react-bootstrap/Card";
 import "./Cards.css";
+import Col from "react-bootstrap/Col";
 
 export const Cards = () => {
   const [card, setCard] = useState([]);
-  const [photo, setPhoto] = useState([]);
 
   useEffect(() => {
     fetch("https://cdn.sozvezdie-tour.ru/content/demo_offers-231105.json")
@@ -14,16 +14,18 @@ export const Cards = () => {
 
   return (
     <>
-      <div>
-        {card.map((res, i) => {
-          return (
-            <ul key={i}>
-              <li>{res.id}</li>
-              <li>{res.title}</li>
-            </ul>
-          );
-        })}
-      </div>
+      {card.map((res) => {
+        return (
+          <Col key={res.id}>
+            <Card style={{ width: "25rem" }}>
+              <Card.Img variant="top" src="card" />
+              <Card.Body>
+                <Card.Title>{res.title}</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+        );
+      })}
     </>
   );
 };
