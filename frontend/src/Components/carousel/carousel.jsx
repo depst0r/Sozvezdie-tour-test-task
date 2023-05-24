edf2639aa2318602e52f02d50c08375f230d69b7
depst0r <depstor90@gmail.com>
2023-05-24 17:19:52 +0300

    next-prev-btn-skider

--- frontend/src/Components/carousel/carousel.jsx
+++ frontend/src/Components/carousel/carousel.jsx
@@ -2,15 +2,21 @@ import React, { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
 
 export const Slider = ({ photo, indexSlide }) => {
+  useEffect(() => {
+    const prev = document.getElementsByClassName("carousel-control-prev")[0];
+    const next = document.getElementsByClassName("carousel-control-next")[0];
+    prev.addEventListener("click", () => {
+      console.log(prev);
+      return console.log("click");
+    });
+  });
   return (
-    <>
-      <Carousel fade variant="dark" activeIndex={indexSlide}>
-        {photo.data?.photoAlbum.map((res) => (
-          <Carousel.Item>
-            <img className="d-block w-100" src={res.photo} alt="slide" />
-          </Carousel.Item>
-        ))}
-      </Carousel>
-    </>
+    <Carousel fade variant="dark" activeIndex={indexSlide}>
+      {photo.data?.photoAlbum.map((res) => (
+        <Carousel.Item>
+          <img className="d-block w-100" src={res.photo} alt="slide" />
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 };
