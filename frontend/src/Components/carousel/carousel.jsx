82418eb93492df1ba6eb4c367128ce2d30f38bf6
depst0r <depstor90@gmail.com>
2023-05-25 15:46:18 +0300

    25.05.23

--- frontend/src/Components/carousel/carousel.jsx
+++ frontend/src/Components/carousel/carousel.jsx
@@ -2,18 +2,22 @@ import React, { useState, useEffect } from "react";
 import Carousel from "react-bootstrap/Carousel";
 
 export const Slider = ({ photo, indexSlide }) => {
-  const [count, setCount] = useState(Number);
+  const [index, setIndex] = useState(Number);
+
+  const getState = (state) => {
+    setIndex(state);
+  };
 
   useEffect(() => {
-    setCount(indexSlide);
+    getState(indexSlide);
   }, []);
 
-  const handleSelect = (selectedIndex, e) => {
-    setCount(selectedIndex);
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
   };
 
   return (
-    <Carousel fade variant="dark" activeIndex={count} onSelect={handleSelect}>
+    <Carousel fade variant="dark" activeIndex={index} onSelect={handleSelect}>
       {photo.data?.photoAlbum.map((res) => (
         <Carousel.Item>
           <img className="d-block w-100" src={res.photo} alt="slide" />
